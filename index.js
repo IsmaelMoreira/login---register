41e55382b8c7a6d57aa208a9d3ee87d3400cfa8c
ismael <moreiraismael14@gmail.com>
2024-03-21 22:33:53 -0300

    Cadastro quase concluido

--- index.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  document.getElementById('linkCadastro').addEventListener('click', toggleForms);
-  document.getElementById('linkLogin').addEventListener('click', toggleForms);
-});
